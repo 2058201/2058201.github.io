735fecb4824f19fee14bc6e867a9832ff7d90e7b
2058201 <87455266+2058201@users.noreply.github.com>
2021-07-24 23:47:06 +0900

    初期コミット

--- js/othello.js
+++ js/othello.js
@@ -1,16 +1,11 @@
-// 変数宣言
-let i, j, x, y;
-
 // 初期設定
 let turn = 0; // 1:黒 -1:白
 let board = document.getElementById('board'); // HTMLのboardを取得
 
 // 盤面を二次元配列で設定
 let board_surface = new Array(8);
-let board_bk = new Array(8);
-for (i = 0; i < 8; i++) {
+for (var i = 0; i < 8; i++) {
     board_surface[i] = new Array(8);
-    board_bk[i] = new Array(8);
 }
 
 // 取得したtableに盤面生成
@@ -20,8 +15,8 @@ makeBoard();
 resetBoard();
 
 // クリック時の処理
-for (x = 0; x < 8; x++) {
-    for (y = 0; y < 8; y++) {
+for (var x = 0; x < 8; x++) {
+    for (var y = 0; y < 8; y++) {
         // 初期設定
         let select_cell = board.rows[x].cells[y];
 
@@ -42,11 +37,11 @@ function makeBoard() {
     // 変数宣言
     let tr, td;
 
-    for (x = 0; x < 8; x++) {
+    for (var x = 0; x < 8; x++) {
         tr = document.createElement("tr"); // HTMLのtrを取得
         board.appendChild(tr);
 
-        for (y = 0; y < 8; y++) {
+        for (var y = 0; y < 8; y++) {
             td = document.createElement("td"); // HTMLのtdを取得
             tr.appendChild(td);
         }
@@ -56,8 +51,8 @@ function makeBoard() {
 // 盤面を初期化する関数
 function resetBoard() {
     // 盤面上の全ての石を初期化
-    for (x = 0; x < 8; x++) {
-        for (y = 0; y < 8; y++) {
+    for (var x = 0; x < 8; x++) {
+        for (var y = 0; y < 8; y++) {
             board_surface[x][y] = 0;
         }
     }
@@ -79,10 +74,10 @@ function resetBoard() {
 // 盤面状況を実際の盤面へ反映させる関数
 function boardSet() {
     // 初期設定
-    let disc = "";
+    let disc = ""; // ●:黒 ○:白
 
-    for (x = 0; x < 8; x++) {
-        for (y = 0; y < 8; y++) {
+    for (var x = 0; x < 8; x++) {
+        for (var y = 0; y < 8; y++) {
             if (board_surface[x][y] == 0) {
                 disc = "";
             } else if (board_surface[x][y] == 1) {
@@ -108,7 +103,7 @@ function chengeTurn() {
 
     if (turn == 0) {
         turn = 1;
-        turn_msg.textContent = "黒の番です。";
+        turn_msg.textContent = "黒の番です．  黒:2  白:2";
         return;
     }
 
@@ -116,26 +111,31 @@ function chengeTurn() {
     turn *= -1;
 
     // 現在の盤面をバックアップ
-    for (x = 0; x < 8; x++) {
-        for (y = 0; y < 8; y++) {
+    let board_bk = new Array(8);
+    for (var i = 0; i < 8; i++) {
+        board_bk[i] = new Array(8);
+    }
+    for (var x = 0; x < 8; x++) {
+        for (var y = 0; y < 8; y++) {
             board_bk[x][y] = board_surface[x][y];
         }
     }
 
-    for (x = 0; x < 8; x++) {
-        for (y = 0; y < 8; y++) {
-            if (board_surface[x][y] == 0) {
-                check_reverse_cnt += checkReverse(x, y);
+    // 盤面上の石の数 & 石を置けるマスの数 を確認 
+    for (var xx = 0; xx < 8; xx++) {
+        for (var yy = 0; yy < 8; yy++) {
+            if (board_surface[xx][yy] == 0) {
+                check_reverse_cnt += checkReverse(xx, yy);
 
                 // バックアップで復元
-                for (i = 0; i < 8; i++) {
-                    for (j = 0; j < 8; j++) {
-                        board_surface[i][j] = board_bk[i][j];
+                for (var ii = 0; ii < 8; ii++) {
+                    for (var jj = 0; jj < 8; jj++) {
+                        board_surface[ii][jj] = board_bk[ii][jj];
                     }
                 }
-            } else if (board_surface[x][y] == 1) {
+            } else if (board_surface[xx][yy] == 1) {
                 black_cnt++;
-            } else if (board_surface[x][y] == -1) {
+            } else if (board_surface[xx][yy] == -1) {
                 white_cnt++;
             }
         }
@@ -144,20 +144,20 @@ function chengeTurn() {
     // 盤面上の石の数が64個 or 黒か白が0個 でゲーム終了
     if (white_cnt + black_cnt == 64 || white_cnt == 0 || black_cnt == 0) {
         if (white_cnt == black_cnt) {
-            alert("勝負は引分です。");
+            alert("勝負の結果は引き分けです．");
         } else if (white_cnt > black_cnt) {
-            alert("勝負は、黒：" + black_cnt + "対、白：" + white_cnt + "で、白の勝ちです。");
+            alert("勝負の結果は，黒:" + black_cnt + " 白:" + white_cnt + " で，白の勝ちです．");
         } else {
-            alert("勝負は、黒：" + black_cnt + "対、白：" + white_cnt + "で、黒の勝ちです。");
+            alert("勝負の結果は，黒:" + black_cnt + " 白:" + white_cnt + " で，黒の勝ちです．");
         }
     } else {
         // 石を置ける場所がない場合，ターン終了
         if (check_reverse_cnt == 0) {
             if (turn == 1) {
-                alert("黒の置ける場所がありません。続けて白の番となります。");
+                alert("黒の置ける場所がありません．白の番となります．");
                 turn *= -1;
             } else if (turn == -1) {
-                alert("白の置ける場所がありません。続けて黒の番となります。");
+                alert("白の置ける場所がありません．黒の番となります．");
                 turn *= -1;
             }
         }
@@ -165,9 +165,9 @@ function chengeTurn() {
 
     // ターンを表示
     if (turn == 1) {
-        turn_msg.textContent = "黒の番です。";
+        turn_msg.textContent = "黒の番です．  黒:" + black_cnt + "  白:" + white_cnt;
     } else if (turn == -1) {
-        turn_msg.textContent = "白の番です。";
+        turn_msg.textContent = "白の番です．  黒:" + black_cnt + "  白:" + white_cnt;
     }
 }
 
@@ -198,8 +198,12 @@ function reverseDiscs(row_num, line_num, add_r, add_l) {
     let l = line_num; // 指定したセルの列
 
     // 現在の盤面をバックアップ
-    for (x = 0; x < 8; x++) {
-        for (y = 0; y < 8; y++) {
+    let board_bk = new Array(8);
+    for (var i = 0; i < 8; i++) {
+        board_bk[i] = new Array(8);
+    }
+    for (var x = 0; x < 8; x++) {
+        for (var y = 0; y < 8; y++) {
             board_bk[x][y] = board_surface[x][y];
         }
     }
@@ -233,9 +237,9 @@ function reverseDiscs(row_num, line_num, add_r, add_l) {
     // 移動先に自分の石がなかったら，裏返した石を元に戻す
     if (reverse_discs_cnt > 0) {
         if (my_disc == 0) {
-            for (x = 0; x < 8; x++) {
-                for (y = 0; y < 8; y++) {
-                    board_surface[x][y] = board_bk[x][y];
+            for (var xx = 0; xx < 8; xx++) {
+                for (var yy = 0; yy < 8; yy++) {
+                    board_surface[xx][yy] = board_bk[xx][yy];
                     reverse_discs_cnt = 0;
                 }
             }
